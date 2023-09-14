27c3fe2fc04fb45b037d91ab9120ae370b9d1d14
Lucas Batista <47784813+lucasdk3@users.noreply.github.com>
2023-09-14 10:50:47 -0300

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,12 +1,12 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-aci-devops"
+  name                = "pepsico-vnet-aci-devops"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "aci-subnet" {
-  name                 = "aci-subnet"
+  name                 = "pepsico-aci-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
