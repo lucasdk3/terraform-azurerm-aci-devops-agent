1467beedb1ada472ce36f1574b474bc6f8fa9602
Lucas Batista <47784813+lucasdk3@users.noreply.github.com>
2023-09-14 10:08:32 -0300

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,18 +1,13 @@
-resource "azurerm_resource_group" "vnet-rg" {
-  name     = "rg-aci-devops-network"
-  location = var.location
-}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-aci-devops"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.vnet-rg.location
-  resource_group_name = azurerm_resource_group.vnet-rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "aci-subnet" {
   name                 = "aci-subnet"
-  resource_group_name  = azurerm_resource_group.vnet-rg.name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 
